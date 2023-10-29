943e17a46afc8a7b710400fc5e71fb0330959c85
Ambspark <ambspark0@gmail.com>
2023-10-29 23:42:56 +0000

    Criando um primeiro teste4. Confuso com os datasets ainda.

---
+++ definitions/source/sources.js
@@ -0,0 +1,23 @@
+declare({
+  schema: dataform.projectConfig.vars.rawDataset,
+  name: "raw_dooca_Brands",
+  description: "Tabela de marcas da plataforma Dooca"
+});
+
+declare({
+  schema: dataform.projectConfig.vars.rawDataset,
+  name: "raw_dooca_Cagegories",
+  description: "Tabela de categorias da plataforma Dooca"
+});
+
+declare({
+  schema: dataform.projectConfig.vars.rawDataset,
+  name: "raw_dooca_Orders",
+  description: "Tabela de pedidos da plataforma Dooca"
+});
+
+declare({
+  schema: dataform.projectConfig.vars.rawDataset,
+  name: "raw_dooca_Products",
+  description: "Tabela de produtos da plataforma Dooca"
+});
