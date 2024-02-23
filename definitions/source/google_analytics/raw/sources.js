42ec04e63791f9f734c0d863fe329b893a532a98
Alexandre Bernardoni <ale@perspectivia.com.br>
2024-02-23 16:09:24 +0000

    Acrescentando KPis de conversão

--- definitions/source/google_analytics/raw/sources.js
+++ definitions/source/google_analytics/raw/sources.js
@@ -4,6 +4,12 @@ NESSE ARQUIVO ESTÃO DECLARADAS TODAS AS ORIGENS DO GOOGLE ANALYTICS
 
 declare({
   schema: dataform.projectConfig.vars.rawDataset,
-  name: "raw_ga_custom_sessions_date",
-  description: "Tabela de sessões do Google Analytics"
+  name: "raw_ga_custom_sessions",
+  description: "Tabela de sessões do Google Analytics, agrupado por dia"
+});
+
+declare({
+  schema: dataform.projectConfig.vars.rawDataset,
+  name: "raw_ga_custom_events",
+  description: "Tabela de eventos do Google Analytics, agrupado por dia"
 });
