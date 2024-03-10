6690fd3417982758d863ce39da696702c6d2a8cb
Alexandre Bernardoni <ale@perspectivia.com.br>
2024-03-10 16:53:38 +0000

    Criando os indicadores de tráfego do GA

--- includes/b4c.js
+++ includes/b4c.js
@@ -1,21 +1,23 @@
 //-----------------------------------------------
-// status_type [Confirmado, Cancelado, Outros]
+// status_type [Aprovado, Cancelado, Pendente Outros]
 function order_status_type(status){
   return `
   case
-    when ${status} in ('Liberado Financeiro', 'Na Expedição', 'Nota Fiscal Emitida', 'Pedido Entregue') then 'Confirmado'
-    when ${status} in ('O pagamento não foi finalizado', 'Pedido cancelado', 'Problema no Boleto', 'Problemas com o Cartão') then 'Cancelado'
-    when ${status} in ('Aguardando Pagamento') then 'Pendente'
+    when ${status} in ('Liberado Financeiro', 'Na Expedição', 'Nota Fiscal Emitida', 'Pedido Entregue', 'Disponível para retirada', 'Em Transporte') then 'Aprovado'
+
+    when ${status} in ('O pagamento não foi finalizado', 'Pedido cancelado', 'Problema no Boleto', 
+      'Problemas com o Cartão', 'Cancelamento Confirmado', 'Pedido Estornado Boleto', 'Pedido Estornado Cartão') then 'Cancelado'
+    when ${status} in ('Aguardando Pagamento', 'Transação em Andamento') then 'Pendente'
     else 'Outros'
   end`; }
 // O que fazer com "Aguradando pagamento"?
 
 //-----------------------------------------------
-// payment_status_type [Confirmado, Cancelado, Pendente, Outros]
+// paymet.status_type [Confirmado, Cancelado, Pendente, Outros]
 function payment_status_type(status){
   return `
   case
-    when ${status} in ('Autorizado', 'Capturado/Confirmado') then 'Confirmado'
+    when ${status} in ('Autorizado', 'Capturado/Confirmado') then 'Aprovado'
     when ${status} in ('Erro', 'Cancelado') then 'Cancelado'
     else 'Outros'
   end`; }
--- includes/nuvemshop.js
+++ includes/nuvemshop.js
@@ -1,9 +1,9 @@
 //-----------------------------------------------
-// status_type [Confirmado, Cancelado, Outros]
+// status_type [Aprovado, Cancelado, Pendente Outros]
 function order_status_type(status){
   return `
   case
-    when ${status} in ('closed') then 'Confirmado'
+    when ${status} in ('closed') then 'Aprovado'
     when ${status} in ('cancelled') then 'Cancelado'
     when ${status} in ('open') then 'Pendente'
     else 'Outros'
@@ -12,11 +12,11 @@ function order_status_type(status){
 
 
 //-----------------------------------------------
-// status_type [Confirmado, Cancelado, Pendente, Outros]
+// paymet.status_type [Confirmado, Cancelado, Pendente, Outros]
 function payment_status_type(status){
   return `
   case
-    when ${status} in ('paid') then 'Confirmado'
+    when ${status} in ('paid') then 'Aprovado'
     when ${status} in ('refunded') then 'Cancelado'
     when ${status} in ('pending') then 'Pendente'
     else 'Outros'
