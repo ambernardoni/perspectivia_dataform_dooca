a1d0e3f8583ce0ddd48435fc23575a7cc2bc55b0
Alexandre Bernardoni <ale@perspectivia.com.br>
2024-07-15 19:26:11 +0000

    Versão 1.0 - Tray

--- includes/dooca.js
+++ includes/dooca.js
@@ -1,3 +1,15 @@
+
+// Essa informação é usada para definir o status geral do pedido na tabelas orders (orders.sqlx)
+// rowConditions: [
+//        'status_type IN ("Aprovado", "Cancelado", "Pendente", "Outros")',
+//        'payment.status_type IN ("Aprovado", "Cancelado", "Pendente", "Outros")',
+//        'status_type_category IN ("Aprovado", "Cancelado", "Pendente")'
+// ]
+// SQL
+//    WHEN payment.status_type = "Cancelado" OR status_type = "Cancelado" THEN "Cancelado"
+//    WHEN payment.status_type = "Aprovado" AND status_type <> "Cancelado" THEN "Aprovado"
+//    ELSE "Pendente" 
+
 //-----------------------------------------------
 // status_type [Aprovado, Cancelado, Pendente Outros]
 function order_status_type(status){
--- includes/tray.js
+++ includes/tray.js
@@ -1,4 +1,3 @@
-
 // Essa informação é usada para definir o status geral do pedido na tabelas orders (orders.sqlx)
 // rowConditions: [
 //        'status_type IN ("Aprovado", "Cancelado", "Pendente", "Outros")',
@@ -10,32 +9,42 @@
 //    WHEN payment.status_type = "Aprovado" AND status_type <> "Cancelado" THEN "Aprovado"
 //    ELSE "Pendente" 
 
+/*
+A ENVIAR VINDI             Pendente
+AGUARDANDO PAGAMENTO       Pendente
+AGUARDANDO VINDI           Pendente
+CANCELADO                  Cancelado
+CANCELADO AUT              Cancelado
+ENVIADO                    Aprovado
+FINALIZADO                 Aprovado
+PROBLEMAS NA ENTREGA       Aprovado
+*/
+
 //-----------------------------------------------
 // status_type [Aprovado, Cancelado, Pendente Outros]
 function order_status_type(status){
   return `
-  case
-    when ${status} in ('closed') then 'Aprovado'
-    when ${status} in ('cancelled') then 'Cancelado'
-    when ${status} in ('open') then 'Pendente'
-    else 'Outros'
-  end`; }
-// O que fazer com "Aguradando pagamento"?
-
+  CASE 
+    WHEN REGEXP_CONTAINS(${status}, r'(AGUARDANDO|A ENVIAR)') THEN 'Pendente'
+    WHEN REGEXP_CONTAINS(${status}, r'(CANCELADO)') THEN 'Cancelado'
+    WHEN REGEXP_CONTAINS(${status}, r'(ENVIADO|FINALIZADO|PROBLEMAS NA ENTREGA)') THEN 'Aprovado'
+    ELSE 'Outros'  -- Optional: to categorize any other statuses
+  END`; }
 
 //-----------------------------------------------
 // paymet.status_type [Confirmado, Cancelado, Pendente, Outros]
+// a Tray não tem fácil o estado do pagamento. Usaremos o estado do pedido, usamos o mesmo status acima
 function payment_status_type(status){
   return `
   case
-    when ${status} in ('paid') then 'Aprovado'
-    when ${status} in ('refunded') then 'Cancelado'
+    when ${status} in ('approved') then 'Aprovado'
+    when ${status} in ('canceled', 'refunded', 'denied', 'expired') then 'Cancelado'
     when ${status} in ('pending') then 'Pendente'
     else 'Outros'
   end`; }
 
-
 module.exports = {
-  order_status_type, 
+  order_status_type,
   payment_status_type
 };
+
